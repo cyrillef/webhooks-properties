d756cd80665309fb126c22abbf77ae6d23529d00
cyrillef <cyrillef@free.fr>
2021-03-02 15:31:18 +0100

    json diffs 1

---
+++ src/tests/json-diff.ts
@@ -0,0 +1,203 @@
+//
+// Copyright (c) Autodesk, Inc. All rights reserved
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM 'AS IS' AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+
+class JsonDiff {
+
+	private lhs: any = null;
+	private rhs: any = null;
+	private keys: string[] = []; // keys to order array of objects, per level.
+	private diffs: Map<string, string> = null;
+
+	constructor(lhs: string | object, rhs: string | object, keys?: string[]) {
+		try {
+			this.keys = keys || [''];
+			// Make copies
+			this.lhs = JSON.parse(typeof lhs === 'string' ? lhs : JSON.stringify(lhs as any));
+			this.rhs = JSON.parse(typeof rhs === 'string' ? rhs : JSON.stringify(rhs as any));
+			this.compare();
+		} catch (ex) {
+		}
+	}
+
+	public get areEquals(): boolean {
+		return (this.diffs === null);
+	}
+
+	protected orderKey(level: number): string {
+		return (this.keys.length >= level + 1 ? this.keys[level] : this.keys[0]);
+	}
+
+	protected compare(bStrict: boolean = false): boolean {
+		if (!this.lhs || !this.rhs)
+			return (false);
+		// First iterate each side, and order elements if an object (we do not reorder arrays, unless their content is objects)
+		if (this.lhs instanceof Object || (Array.isArray(this.lhs) && this.lhs.length > 0 && typeof this.lhs[0] === 'object'))
+			this.reorderObject(this.lhs, 0);
+		if (this.rhs instanceof Object || (Array.isArray(this.rhs) && this.rhs.length > 0 && typeof this.rhs[0] === 'object'))
+			this.reorderObject(this.rhs, 0);
+
+		// Atomics and objects are easy to compare, Arrays are more complicated
+		this.compareObjects(this.lhs, this.rhs, '//');
+
+		return (this.areEquals);
+	}
+
+	protected reorderObject(obj: any, level: number): any {
+		const self = this;
+		if (typeof obj !== 'object')
+			return (obj);
+		if (Array.isArray(obj))
+			return (this.reorderArray(obj, level));
+
+		const keys = Object.keys(obj).sort();
+		keys.map((key: string): void => {
+			const item = obj[key];
+			delete obj[key];
+			obj[key] = self.reorderObject(item, level + 1);
+		});
+		return (obj);
+	}
+
+	protected reorderArray(obj: any, level: number): any {
+		const self = this;
+		if (!Array.isArray(obj) || obj.length === 0 || typeof obj[0] !== 'object') // we do not reorder arrays, unless their content is objects
+			return (obj);
+
+		if (Array.isArray(obj[0]))
+			return (obj.map((elt: any): any => self.reorderArray(elt, level + 1)));
+
+		if (!this.keys || this.keys.length === 0)
+			return (obj);
+		const orderKey: string = this.orderKey(level);
+		if ( orderKey !== '')
+			obj = obj.sort((a: any, b: any): number => {
+				if (a[orderKey] === b[orderKey])
+					return (0);
+				if (a[orderKey] < b[orderKey])
+					return (-1);
+				return (1);
+			});
+		obj = obj.map((elt: any): any => self.reorderObject(elt, level + 1));
+
+		return (obj);
+	}
+
+	protected compareObjects(lhs: any, rhs: any, path: string): boolean {
+		const self = this;
+		if (lhs === undefined)
+			return (this.logDiff(path, 'missing on left'));
+		if (rhs === undefined)
+			return (this.logDiff(path, 'missing on right'));
+		if (lhs === rhs) // atomics
+			return (true);
+		// they must have the exact same prototype chain, the closest we can do is test there constructor.
+		if (lhs.constructor !== rhs.constructor)
+			return (this.logDiff(path, 'not of the same type'));
+		if (typeof lhs !== 'object' || typeof rhs !== 'object') // object or array
+			return (this.logDiff(path, `not equal`));
+
+		if (Array.isArray(lhs))
+			return (this.compareArrays(lhs, rhs, path));
+
+		// Compare left to right
+		const Left2Right: boolean = Object.keys(lhs).sort()
+			.map((key: string): boolean => self.compareObjects(lhs[key], rhs[key], `${path}${key}/`))
+			.filter((res: boolean) => res)
+			.length > 0;
+		// Compare right to left ( but no repeat copmaraisons, we only need to detect missing keys )
+		const Right2Left: boolean = Object.keys(rhs).sort()
+			.map((key: string): boolean => (!lhs.hasOwnProperty(key) ? self.logDiff(`${path}${key}/`, 'missing on left') : true))
+			.filter((res: boolean) => res)
+			.length > 0;
+
+		return (Left2Right && Right2Left);
+	}
+
+	protected compareArrays(lhs: any, rhs: any, path: string): boolean {
+		const self = this;
+		if (!Array.isArray(lhs) || !Array.isArray(lhs))
+			return (false); // should not happen
+
+		if (lhs.length > 0 && Array.isArray(lhs[0])) {
+			return (lhs
+				.map((elt: any, index: number): any => self.compareObjects(elt, rhs[index], `${path}${index}/`))
+				.filter((res: boolean) => res)
+				.length > 0
+			);
+		}
+
+		if (lhs.length > 0 && typeof lhs[0] === 'object') {
+			const level: number = path.split('/').length - 4;
+			const orderKey: string = this.orderKey(level);
+			lhs.map((elt: any, index: number): any => {
+				let findElt: any = -1;
+				if ( orderKey !== '' )
+					findElt = rhs.filter((relt: any): boolean => elt[orderKey] === relt[orderKey]);
+				else
+					findElt = rhs.length > index ? [rhs[index]] : [];
+				
+				if ( !findElt || findElt.length === 0 )
+					return (self.logDiff(`${path}[${orderKey}=${elt[orderKey || index]}]`, 'missing on right'));
+				return (self.compareObjects(elt, findElt[0], `${path}[${orderKey || index}=${elt[orderKey || index]}]`));
+			});
+		}
+		if (rhs.length > 0 && typeof rhs[0] === 'object') {
+			const level: number = path.split('/').length - 4;
+			const orderKey: string = this.orderKey(level);
+			rhs.map((elt: any, index: number): any => {
+				let findElt: any = -1;
+				if (orderKey !== '')
+					findElt = lhs.filter((lelt: any): boolean => elt[orderKey] === lelt[orderKey]);
+				else
+					findElt = lhs.length > index ? [lhs[index]] : [];
+
+				if (!findElt || findElt.length === 0)
+					return (self.logDiff(`${path}[${orderKey}=${elt[orderKey || index]}]`, 'missing on left'));
+				return (true); // no need to compare again
+			});
+		}
+
+		// good enough for atomics
+		if (lhs.length !== rhs.length)
+			this.logDiff(path, 'warning: arrays do not have the same length');
+		if (JSON.stringify(lhs) !== JSON.stringify(rhs))
+			return (this.logDiff(path, 'arrays content are different'));
+		return (true);
+	}
+
+	protected logDiff(path: string, msg: string): boolean {
+		if (this.diffs === null)
+			this.diffs = new Map<string, string>();
+
+		if (!this.diffs.get(path))
+			this.diffs.set(path, msg);
+
+		return (false); // always
+	}
+
+	public toString(indent: number = 0): string {
+		if ( this.diffs === null )
+			return ('');
+		const obj: any = {}
+		for (let [k, v] of this.diffs.entries())
+			obj[k] = v;
+		return (JSON.stringify(obj, null, indent));
+	}
+
+}
+
+export default JsonDiff
--- src/tests/tree-properties.ts
+++ src/tests/tree-properties.ts
@@ -22,47 +22,42 @@ import AppSettings from './app-settings';
 import * as util from 'util';
 import * as _fs from 'fs';
 import * as _path from 'path';
+import JsonDiff from './json-diff';
+import * as diffx from 'deep-diff';
 
 const _fsWriteFile = util.promisify(_fs.writeFile);
 
-(Object as any).equals = (x: any, y: any) => {
-	if (x === y)
+(Object as any).equals = (x: any, y: any, path: string = './') => {
+	if (x === y) // if both x and y are null or undefined and exactly the same
 		return (true);
-	// if both x and y are null or undefined and exactly the same
 
-	if (!(x instanceof Object) || !(y instanceof Object))
-		return (false);
 	// if they are not strictly equal, they both need to be Objects
-
+	if (!(x instanceof Object) || !(y instanceof Object))
+		return (console.log(`- diff @ ${path}`), false);
+	// they must have the exact same prototype chain, the closest we can do is test there constructor.
 	if (x.constructor !== y.constructor)
-		return (false);
-	// they must have the exact same prototype chain, the closest we can do is
-	// test there constructor.
+		return (console.log(`- diff @ ${path}`), false);
 
-	for (var p in x) {
-		if (!x.hasOwnProperty(p)) continue;
+	for (let p in x) {
 		// other properties were tested using x.constructor === y.constructor
-
-		if (!y.hasOwnProperty(p))
-			return (false);
-		// allows to compare x[ p ] and y[ p ] when set to undefined
-
-		if (x[p] === y[p])
+		if (!x.hasOwnProperty(p))
 			continue;
-		// if they have the same strict value or identity then they are equal
-
-		if (typeof (x[p]) !== "object")
-			return (false);
+		// allows to compare x[ p ] and y[ p ] when set to undefined
+		if (!y.hasOwnProperty(p))
+			return (console.log(`- diff @ ${path}`), false);
+		if (x[p] === y[p]) // if they have the same strict value or identity then they are equal
+			continue;
 		// Numbers, Strings, Functions, Booleans must be strictly equal
-
-		if (!(Object as any).equals(x[p], y[p]))
-			return (false);
+		if (typeof (x[p]) !== 'object')
+			return (console.log(`- diff @ ${path}`), false);
 		// Objects and Arrays must be tested recursively
+		if (!(Object as any).equals(x[p], y[p], `${path}${p}/`))
+			return (/*console.log(`${path}${p} are different`),*/ false);
 	}
 
-	for (p in y) {
+	for (let p in y) {
 		if (y.hasOwnProperty(p) && !x.hasOwnProperty(p))
-			return (false);
+			return (console.log(`- diff @ ${path}`), false);
 		// allows x[ p ] to be set to undefined
 	}
 	return (true);
@@ -84,10 +79,10 @@ class TreePropertiesTestsController {
 		pier9: { // oZZ0CN7qXTGAiqSbmEhLlmYcKXt0YVoU
 			urn: 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y3lyaWxsZS1tb2RlbHMvUDlfTWFjaGluZVNob3BfRmluYWwucnZ0',
 			guids: [
-				'ee578c34-41d4-83e7-fd72-1c18a453c3b9', // 3d role: "graphics", mime: "application/autodesk-svf", type: "resource"
-				//'ee578c34-41d4-83e7-fd72-1c18a453c3b9', // 3d role: "graphics", mime: "application/autodesk-svf2", type: "resource"
-				'6fda4fe6-0ceb-4525-a86d-20be4000dab5', // 2d role: "graphics", mime: "application/autodesk-f2d", type: "resource",
-				'e67f2035-8010-3ff5-e399-b9c9217c2366', // 2d role: "graphics", mime: "application/autodesk-f2d", type: "resource",
+				'ee578c34-41d4-83e7-fd72-1c18a453c3b9', // 3d role: 'graphics', mime: 'application/autodesk-svf', type: 'resource'
+				//'ee578c34-41d4-83e7-fd72-1c18a453c3b9', // 3d role: 'graphics', mime: 'application/autodesk-svf2', type: 'resource'
+				'6fda4fe6-0ceb-4525-a86d-20be4000dab5', // 2d role: 'graphics', mime: 'application/autodesk-f2d', type: 'resource',
+				'e67f2035-8010-3ff5-e399-b9c9217c2366', // 2d role: 'graphics', mime: 'application/autodesk-f2d', type: 'resource',
 			],
 			objid: 1,
 		},
@@ -117,24 +112,26 @@ class TreePropertiesTestsController {
 			const obj0 = JSON.parse(results[0].text);
 			const obj1 = JSON.parse(results[1].text);
 
-			obj1.data.objects[0].objects = obj1.data.objects[0].objects.filter ((elt: any): boolean => {
-				return (
-					elt.name !== 'Schedule Graphics' 
-					&& elt.name !== 'Title Blocks' 
-					&& elt.name !== ''
+			// obj1.data.objects[0].objects = obj1.data.objects[0].objects.filter ((elt: any): boolean => {
+			// 	return (
+			// 		elt.name !== 'Schedule Graphics' 
+			// 		&& elt.name !== 'Title Blocks' 
+			// 		&& elt.name !== ''
 
-					&& elt.name !== 'Survey Point'
-					&& elt.name !== 'Project Base Point'
-				);
-			});
+			// 		&& elt.name !== 'Survey Point'
+			// 		&& elt.name !== 'Project Base Point'
+			// 	);
+			// });
 
+			const cmp: JsonDiff = new JsonDiff(obj0, obj1, ['objectid']);
+			if (cmp.areEquals)
 			//if (JSON.stringify(obj0) === JSON.stringify(obj1))
-			if ((Object as any).equals(obj0, obj1))
+			//if ((Object as any).equals(obj0, obj1))
 				console.log(`${message} identical`);
 			else
-				console.warn(`${message} different`);
+				console.warn(`${message} different`, cmp.toString(4));
 
-			if ( fn) {
+			if (fn) {
 				await _fsWriteFile(`${fn}.json`, Buffer.from(JSON.stringify(obj0, null, 4)));
 				await _fsWriteFile(`${fn}-db.json`, Buffer.from(JSON.stringify(obj1, null, 4)));
 			}
@@ -152,6 +149,154 @@ class TreePropertiesTestsController {
 		const urn: string = TreePropertiesTestsController.objects.pier9.urn;
 		const guid: string = TreePropertiesTestsController.objects.pier9.guids[index];
 		await this.request(`http://localhost:3001/tree/${urn}/guids/${guid}`, `tree ${urn} ${guid}`, _path.resolve(__dirname, `tree-${urn}-${guid}`));
+	}
+
+	public async test(index: number = 0): Promise<void> {
+		const json0: any = {
+			key: 1,
+			test1: 1,
+			test2: 'test',
+			test3: [1, 3, 5, 2],
+			test4: {
+				key: 4,
+				test41: 4,
+				test42: 'test4',
+				test43: [1, 3, 5, 2],
+			},
+			test5: [
+				{
+					key: 5,
+					test51: 5,
+					test52: 'test5',
+					test53: [1, 3, 5, 2],
+				},
+				{
+					key: 6,
+					test61: 6,
+					test62: 'test6',
+					test63: [1, 3, 5, 2],
+				},
+				{
+					key: 7,
+					test71: 7,
+					test72: 'test7',
+					test73: [1, 3, 5, 2],
+				},
+				{
+					key: 8,
+					test81: 8,
+				},
+			],
+			testmissing: 9,
+		};
+		const json1: any = {
+			key: 1,
+			test2: 'test',
+			test4: {
+				key: 4,
+				test41: 4,
+				test43: [1, 3, 5, 2],
+				test42: 'test4',
+			},
+			test1: 1,
+			test5: [
+				{
+					key: 6,
+					test62: 'test6',
+					test63: [1, 3, 5, 2],
+					test61: 6,
+				},
+				{
+					test51: 5,
+					test53: [1, 3, 5, 2],
+					key: 5,
+					test52: 'test5',
+				},
+				{
+					test73: [1, 3, 5, 2],
+					test71: 7,
+					test72: 'test7',
+					key: 7,
+				},
+				{
+					key: 8,
+					test81: 8,
+				},
+			],
+			test3: [1, 3, 5, 2],
+			testmissing: 9,
+		};
+		const json2: any = {
+			key: 1,
+			test2: 'test_', // < not equal
+			test4: {
+				key: 4,
+				test41: 44, // < not equal
+				test43: [1, 3, 5, 2],
+				test42: 'test4',
+			},
+			test1: 1,
+			test5: [
+				{
+					key: 6,
+					test62: 'test6_', // < not equal
+					//test63: [1, 3, 5, 2], // < missing on right
+					test61: 66, // < not equal
+				},
+				{
+					test51: 5,
+					test53: [1, 8, 5, 2], // < not equal
+					key: 5,
+					test52: 'test5',
+				},
+				{
+					test73: [1, 3, 5, 2],
+					test71: '7', // < not of the same type
+					test72: 'test7',
+					key: 7,
+					test75: 0, // < missing on left
+				},
+				{
+					key: 9,
+					test91: 9,
+				},
+			],
+			test3: [1, 3, 55, 2], // < not equal
+			testnew: 9, // < missing on left
+		};
+		const diff2 = {
+			'//test2/': 'not equal',
+			'//test3/': 'arrays content are different',
+			'//test4/test41/': 'not equal',
+			'//test5/[key=5]test53/': 'arrays content are different',
+			'//test5/[key=6]test61/': 'not equal',
+			'//test5/[key=6]test62/': 'not equal',
+			'//test5/[key=6]test63/': 'missing on right',
+			'//test5/[key=7]test71/': 'not of the same type',
+			'//test5/[key=7]test75/': 'missing on left',
+			"//test5/[key=8]": "missing on right",
+			"//test5/[key=9]": "missing on left",
+			"//test5/": "arrays content are different",
+			'//testmissing/': 'missing on right',
+			'//testnew/': 'missing on left',
+		};
+
+		const cmp1: JsonDiff = new JsonDiff(json0, json1, ['key']);
+		if (!cmp1.areEquals)
+			console.log(cmp1.toString(4));
+
+		const cmp2: JsonDiff = new JsonDiff(json0, json2, ['key']);
+		if (!cmp2.areEquals) {
+			console.log(cmp2.toString(4));
+			//console.log(JSON.stringify(diff2, null, 4));
+			if (JSON.stringify(diff2) !== cmp2.toString())
+				console.log('I was expecting', JSON.stringify(diff2, null, 4));
+		}
+
+		// const cmp3 = diffx.diff(json0, json1);
+		// const cmp4 = diffx.diff(json0, json2);
+
+		let g = 0;
 	}
 
 }
